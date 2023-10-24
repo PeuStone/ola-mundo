102de4aa39583fd42dbdc386b3fab127c1716269
PeuStone <pedrohmotaaa@gmail.com>
2023-10-23 22:13:52 -0300

    Correção de espaço sobressalente

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
 
-export default function Post () {
+export default function Post() {
     const parametros = useParams()
 
     console.log(parametros);
