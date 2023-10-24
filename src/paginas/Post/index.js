16a0f7b8d47b0528803125b52e4dbeea28e6b55f
PeuStone <pedrohmotaaa@gmail.com>
2023-10-23 23:13:07 -0300

    Estilização do conteudo dos Posts

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom"
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
+import ReactMarkdowm from "react-markdown";
+import './Post.css';
 
 export default function Post() {
     const parametros = useParams()
@@ -15,7 +17,11 @@ export default function Post() {
             fotoCapa={`/assets/posts/${post.id}/capa.png`}
             titulo={post.titulo}
         >
-            Texto...
+            <div className="post-markdown-container">
+                <ReactMarkdowm>
+                    {post.texto}
+                </ReactMarkdowm>
+            </div>
         </PostModelo>
     )
 }
