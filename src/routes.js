85c72258ef4e0048d3d597aa7957ed4f4cce96ac
PeuStone <pedrohmotaaa@gmail.com>
2023-10-23 22:13:06 -0300

    Criação da pagina dos Posts do projeto

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
 
 
 function AppRoutes() {
@@ -16,6 +17,7 @@ function AppRoutes() {
 
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
 
         <Route path="*" element={<div>Página não encontrada</div>} />
